80113ac4d04a923f7a2ff92a3fd0266d4bb70185
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-04-04 18:32:28 +0530

    Introduce functioning of waste piles

--- src/App.js
+++ src/App.js
@@ -48,22 +48,42 @@ class App extends Component {
     return toRenderPiles;
   }
 
-  handleWastePile() {}
+  resetWastePile() {
+    if (this.state.wastePiles[1].length) return;
+    const faceOnWastePile = this.state.wastePiles[2];
+    faceOnWastePile.reverse();
+    this.setState({ wastePiles: { 1: faceOnWastePile, 2: [] } });
+  }
+
+  handleWastePile(event) {
+    const wastePile = this.state.wastePiles[1];
+    const faceOnWastePile = this.state.wastePiles[2];
+    const clickedCard = wastePile[wastePile.length - 1];
+    wastePile.pop();
+    faceOnWastePile.push(clickedCard);
+    this.setState({ wastePiles: { 1: wastePile, 2: faceOnWastePile } });
+  }
 
   generateWastePiles() {
     const wastePiles = this.state.wastePiles;
-    const toRenderWastePiles = Object.keys(wastePiles).map(wastePile => {
-      const toRenderPile = wastePiles[wastePile].map(card => (
-        <div
-          className="waste-pile-card"
-          style={{ color: card.color }}
-          dangerouslySetInnerHTML={{
-            __html: `${card.getUnicode()}`
-          }}
-        />
-      ));
-      return <div className="waste-pile">{toRenderPile}</div>;
-    });
+    const toRenderWastePiles = Object.keys(wastePiles).map(
+      (wastePile, index) => {
+        let onclick = this.resetWastePile.bind(this);
+        if (index === 0) onclick = this.handleWastePile.bind(this);
+
+        const toRenderPile = wastePiles[wastePile].map(card => (
+          <div
+            onClick={onclick}
+            className="waste-pile-card"
+            style={{ color: card.color }}
+            dangerouslySetInnerHTML={{
+              __html: `${card.getUnicode()}`
+            }}
+          />
+        ));
+        return <div className="waste-pile">{toRenderPile}</div>;
+      }
+    );
     return <div className="waste-piles">{toRenderWastePiles}</div>;
   }
 
