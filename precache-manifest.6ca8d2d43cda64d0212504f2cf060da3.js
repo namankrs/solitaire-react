a5d63bc09597b86354d5360e24265219ee804209
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-04-13 19:07:17 +0530

    Updates

---
+++ precache-manifest.6ca8d2d43cda64d0212504f2cf060da3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94631498ad1bf68c33c8",
+    "url": "/static/js/main.2413db86.chunk.js"
+  },
+  {
+    "revision": "8c650a197ae475b7cef1",
+    "url": "/static/js/2.3c610494.chunk.js"
+  },
+  {
+    "revision": "94631498ad1bf68c33c8",
+    "url": "/static/css/main.2203ee59.chunk.css"
+  },
+  {
+    "revision": "c033f02b2288601ba63ecd25530d3107",
+    "url": "/index.html"
+  }
+];
