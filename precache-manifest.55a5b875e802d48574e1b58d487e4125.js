72bf2d6b22744a29f71b8a1f762e07613d6092cc
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-04-15 17:01:52 +0530

    Updates

---
+++ precache-manifest.55a5b875e802d48574e1b58d487e4125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "423a1cf31494fdd5629d",
+    "url": "/solitaire-react/static/js/runtime~main.a8e704d3.js"
+  },
+  {
+    "revision": "94631498ad1bf68c33c8",
+    "url": "/solitaire-react/static/js/main.2413db86.chunk.js"
+  },
+  {
+    "revision": "8c650a197ae475b7cef1",
+    "url": "/solitaire-react/static/js/2.3c610494.chunk.js"
+  },
+  {
+    "revision": "94631498ad1bf68c33c8",
+    "url": "/solitaire-react/static/css/main.2203ee59.chunk.css"
+  },
+  {
+    "revision": "c364854ab7cc655479777197fce8b5dc",
+    "url": "/solitaire-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ca8d2d43cda64d0212504f2cf060da3.js"
+  "/solitaire-react/precache-manifest.55a5b875e802d48574e1b58d487e4125.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/solitaire-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
